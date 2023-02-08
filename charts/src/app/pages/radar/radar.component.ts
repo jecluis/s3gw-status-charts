ec9ea3ee928676d5ebb594f0e7cab3578d43dac7
Joao Eduardo Luis <joao@suse.com>
2023-02-08 22:25:37 -0100

    stash again
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

--- charts/src/app/pages/radar/radar.component.ts
+++ charts/src/app/pages/radar/radar.component.ts
@@ -16,58 +16,377 @@ import {
   Component,
   ElementRef,
   OnChanges,
+  OnDestroy,
   OnInit,
   SimpleChanges,
   ViewChild,
 } from "@angular/core";
 
 import * as Plotly from "plotly.js-dist-min";
+import { Subscription } from "rxjs";
+import {
+  Epics,
+  GithubService,
+  Issues,
+  IssuesByState,
+} from "src/app/shared/services/github.service";
 
 @Component({
   selector: "s3gw-radar",
   templateUrl: "./radar.component.html",
   styleUrls: ["./radar.component.scss"],
 })
-export class RadarComponent implements OnInit, OnChanges {
-  @ViewChild("plot", { static: true })
-  public plotElement!: ElementRef;
+export class RadarComponent implements OnInit, OnChanges, OnDestroy {
+  @ViewChild("epicsPlot", { static: true })
+  public epicsPlotElement!: ElementRef;
 
-  private layout!: Partial<Plotly.Layout>;
-  private data: Plotly.Data[] = [];
+  @ViewChild("kindsPlot", { static: true })
+  public kindsPlotElement!: ElementRef;
 
-  public constructor() {}
+  @ViewChild("areasPlot", { static: true })
+  public areasPlotElement!: ElementRef;
+
+  @ViewChild("byStateKindsPlot", { static: true })
+  public byStateKindsPlotElement!: ElementRef;
+  @ViewChild("byStateAreaPlot", { static: true })
+  public byStateAreaPlotElement!: ElementRef;
+
+  private epicsLayout!: Partial<Plotly.Layout>;
+  private epicsData: Plotly.Data[] = [];
+
+  private issuesByKindLayout!: Partial<Plotly.Layout>;
+  private issuesByKindData: Plotly.Data[] = [];
+
+  private issuesByAreaLayout!: Partial<Plotly.Layout>;
+  private issuesByAreaData: Plotly.Data[] = [];
+
+  private stateIssuesByKindLayout!: Partial<Plotly.Layout>;
+  private stateIssuesByKindData: Plotly.Data[] = [];
+
+  private stateIssuesByAreaLayout!: Partial<Plotly.Layout>;
+  private stateIssuesByAreaData: Plotly.Data[] = [];
+
+  private epicsSubscription?: Subscription;
+  private issuesSubscription?: Subscription;
+  private issuesByStateSubscription?: Subscription;
+
+  public notEnoughEpics = false;
+  public notEnoughKinds = false;
+  public notEnoughAreas = false;
+  public notEnoughKindsByState = false;
+  public notEnoughAreasByState = false;
+
+  public constructor(private ghSvc: GithubService) {}
 
   public ngOnInit(): void {
-    this.layout = {
-      polar: {
-        radialaxis: {
-          visible: true,
-          range: [0, 50],
-        },
+    this.epicsSubscription = this.ghSvc.getEpics().subscribe({
+      next: (epics: Epics) => {
+        this.updateEpics(epics);
       },
-    };
-    this.data = [
-      {
-        type: "scatterpolar",
-        r: [39, 28, 8, 7, 28, 39],
-        theta: ["A", "B", "C", "D", "E", "A"],
-        fill: "toself",
-        name: "group A",
-      },
-    ];
+    });
 
-    this.create().then(() => {});
+    this.issuesSubscription = this.ghSvc.getIssues().subscribe({
+      next: (issues: Issues) => {
+        this.updateIssues(issues);
+      },
+    });
+
+    this.issuesByStateSubscription = this.ghSvc.getIssuesByState().subscribe({
+      next: (issues: IssuesByState) => {
+        this.updateIssuesByState(issues);
+      },
+    });
+  }
+
+  public ngOnDestroy(): void {
+    if (!!this.epicsSubscription) {
+      this.epicsSubscription.unsubscribe();
+    }
+    if (!!this.issuesSubscription) {
+      this.issuesSubscription.unsubscribe();
+    }
+    if (!!this.issuesByStateSubscription) {
+      this.issuesByStateSubscription.unsubscribe();
+    }
   }
 
   public ngOnChanges(changes: SimpleChanges): void {
     console.debug("plot changes: ", changes);
   }
 
-  private create(): Promise<Plotly.PlotlyHTMLElement> {
+  private createEpics(): Promise<Plotly.PlotlyHTMLElement> {
     return Plotly.newPlot(
-      this.plotElement.nativeElement,
-      this.data,
-      this.layout,
+      this.epicsPlotElement.nativeElement,
+      this.epicsData,
+      this.epicsLayout,
     );
+  }
+
+  private createKinds(): Promise<Plotly.PlotlyHTMLElement> {
+    return Plotly.newPlot(
+      this.kindsPlotElement.nativeElement,
+      this.issuesByKindData,
+      this.issuesByKindLayout,
+    );
+  }
+
+  private createAreas(): Promise<Plotly.PlotlyHTMLElement> {
+    return Plotly.newPlot(
+      this.areasPlotElement.nativeElement,
+      this.issuesByAreaData,
+      this.issuesByAreaLayout,
+    );
+  }
+
+  private createByStateKinds(): Promise<Plotly.PlotlyHTMLElement> {
+    return Plotly.newPlot(
+      this.byStateKindsPlotElement.nativeElement,
+      this.stateIssuesByKindData,
+      this.stateIssuesByKindLayout,
+    );
+  }
+
+  private createByStateAreas(): Promise<Plotly.PlotlyHTMLElement> {
+    return Plotly.newPlot(
+      this.byStateAreaPlotElement.nativeElement,
+      this.stateIssuesByAreaData,
+      this.stateIssuesByAreaLayout,
+    );
+  }
+
+  private updateEpics(epics: Epics) {
+    if (epics.epics.length < 3) {
+      this.notEnoughEpics = true;
+      console.log("not enough data points");
+      return;
+    }
+
+    const theta = [...epics.epics, epics.epics[0]];
+    console.log("theta: ", theta);
+
+    let r: number[] = [];
+    let highest: number = 0;
+    theta.forEach((name: string) => {
+      const n = epics.issuesByEpic[name].length;
+      highest = Math.max(highest, n);
+      r.push(n);
+    });
+    console.log("r: ", r);
+
+    this.epicsLayout = {
+      polar: {
+        radialaxis: {
+          visible: true,
+          range: [0, highest],
+        },
+      },
+    };
+    this.epicsData = [
+      {
+        type: "scatterpolar",
+        r: r,
+        theta: theta,
+        fill: "toself",
+        name: "group A",
+      },
+    ];
+
+    this.createEpics().then(() => {});
+  }
+
+  private updateIssues(issues: Issues) {
+    if (issues.kinds.length >= 3) {
+      this.updateKinds(issues);
+    }
+    if (issues.areas.length >= 3) {
+      this.updateAreas(issues);
+    }
+  }
+
+  private updateKinds(issues: Issues) {
+    const kinds = [...issues.kinds, issues.kinds[0]];
+    let r: number[] = [];
+    let highest: number = 0;
+    kinds.forEach((name: string) => {
+      const n = issues.byKind[name].length;
+      highest = Math.max(highest, n);
+      r.push(n);
+    });
+
+    this.issuesByKindLayout = {
+      polar: {
+        radialaxis: {
+          visible: true,
+          range: [0, highest],
+        },
+      },
+    };
+    this.issuesByKindData = [
+      {
+        type: "scatterpolar",
+        r: r,
+        theta: kinds,
+        fill: "toself",
+        name: "issue kind",
+      },
+    ];
+
+    this.createKinds().then(() => {});
+  }
+
+  private updateAreas(issues: Issues) {
+    const areas = [...issues.areas, issues.areas[0]];
+    let r: number[] = [];
+    let highest: number = 0;
+    areas.forEach((name: string) => {
+      const n = issues.byArea[name].length;
+      highest = Math.max(highest, n);
+      r.push(n);
+    });
+
+    this.issuesByAreaLayout = {
+      polar: {
+        radialaxis: {
+          visible: true,
+          range: [0, highest],
+        },
+      },
+    };
+    this.issuesByAreaData = [
+      {
+        type: "scatterpolar",
+        r: r,
+        theta: areas,
+        fill: "toself",
+        name: "issue kind",
+      },
+    ];
+
+    this.createAreas().then(() => {});
+  }
+
+  private updateIssuesByState(issues: IssuesByState) {
+    this.updateByStateKinds(issues);
+    this.updateByStateAreas(issues);
+  }
+
+  private updateByStateKinds(issues: IssuesByState) {
+    const kinds = [...issues.open.kinds];
+    issues.closed.kinds.forEach((v: string) => {
+      if (kinds.includes(v)) {
+        return;
+      }
+      kinds.push(v);
+    });
+    if (kinds.length === 0) {
+      this.notEnoughKindsByState = true;
+      console.log("not enough kinds by state");
+      return;
+    }
+    kinds.push(kinds[0]);
+
+    const open_r: number[] = [];
+    const closed_r: number[] = [];
+    let highest = 0;
+    kinds.forEach((name: string) => {
+      let open_n = 0;
+      if (name in issues.open.byKind) {
+        open_n = issues.open.byKind[name].length;
+      }
+      let closed_n = 0;
+      if (name in issues.closed.byKind) {
+        closed_n = issues.closed.byKind[name].length;
+      }
+      highest = Math.max(highest, open_n, closed_n);
+      open_r.push(open_n);
+      closed_r.push(closed_n);
+    });
+
+    this.stateIssuesByKindLayout = {
+      polar: {
+        radialaxis: {
+          visible: true,
+          range: [0, highest],
+        },
+      },
+    };
+    this.stateIssuesByKindData = [
+      {
+        type: "scatterpolar",
+        r: open_r,
+        theta: kinds,
+        fill: "toself",
+        name: "open issues",
+      },
+      {
+        type: "scatterpolar",
+        r: closed_r,
+        theta: kinds,
+        fill: "toself",
+        name: "closed issues",
+      },
+    ];
+
+    this.createByStateKinds().then(() => {});
+  }
+
+  private updateByStateAreas(issues: IssuesByState) {
+    const areas = [...issues.open.areas];
+    issues.closed.areas.forEach((v: string) => {
+      if (areas.includes(v)) {
+        return;
+      }
+      areas.push(v);
+    });
+    if (areas.length === 0) {
+      this.notEnoughAreasByState = true;
+      console.log("not enough areas by state");
+      return;
+    }
+    areas.push(areas[0]);
+
+    const open_r: number[] = [];
+    const closed_r: number[] = [];
+    let highest = 0;
+    areas.forEach((name: string) => {
+      let open_n = 0;
+      if (name in issues.open.byArea) {
+        open_n = issues.open.byArea[name].length;
+      }
+      let closed_n = 0;
+      if (name in issues.closed.byArea) {
+        closed_n = issues.closed.byArea[name].length;
+      }
+      highest = Math.max(highest, open_n, closed_n);
+      open_r.push(open_n);
+      closed_r.push(closed_n);
+    });
+
+    this.stateIssuesByAreaLayout = {
+      polar: {
+        radialaxis: {
+          visible: true,
+          range: [0, highest],
+        },
+      },
+    };
+    this.stateIssuesByAreaData = [
+      {
+        type: "scatterpolar",
+        r: open_r,
+        theta: areas,
+        fill: "toself",
+        name: "open issues",
+      },
+      {
+        type: "scatterpolar",
+        r: closed_r,
+        theta: areas,
+        fill: "toself",
+        name: "closed issues",
+      },
+    ];
+
+    this.createByStateAreas().then(() => {});
   }
 }
--- charts/src/app/shared/services/github.service.ts
+++ charts/src/app/shared/services/github.service.ts
@@ -14,34 +14,442 @@
 
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { graphql, GraphQlQueryResponseData } from "@octokit/graphql";
+import { BehaviorSubject } from "rxjs";
 
-type GithubLabel = {
-  id: number;
-  node_id: string;
-  url: string;
+type GithubProjectItem = {
+  fieldValueByName?: {
+    name: string;
+  };
+};
+
+type GithubSearchNode = {
+  title: string;
+  number: number;
+  projectItems: {
+    nodes: GithubProjectItem[];
+  };
+};
+
+type GithubSearchResult = {
+  search: {
+    issueCount: number;
+    nodes: GithubSearchNode[];
+  };
+};
+
+export type EpicIssueEntry = {
+  number: number;
+  title: string;
+  epic: string;
+};
+
+export type IssueByEpicMap = { [id: string]: EpicIssueEntry[] };
+
+export type Epics = {
+  epics: string[];
+  issuesByEpic: IssueByEpicMap;
+};
+
+type GithubIssueLabel = {
   name: string;
-  description: string;
   color: string;
-  default: boolean;
+};
+
+type GithubIssuesNode = {
+  state?: String;
+  title: string;
+  number: number;
+  labels: { nodes: GithubIssueLabel[] };
+  milestone?: { title: string };
+};
+
+type GithubIssuesResult = {
+  search: {
+    issueCount: number;
+    nodes: GithubIssuesNode[];
+  };
+};
+
+export type IssueEntry = {
+  title: string;
+  number: number;
+  kind: string;
+  area: string[];
+};
+
+export type Issues = {
+  kinds: string[];
+  areas: string[];
+  byKind: { [id: string]: IssueEntry[] };
+  byArea: { [id: string]: IssueEntry[] };
+};
+
+type GithubAllIssuesResult = {
+  search: {
+    issueCount: number;
+    pageInfo: {
+      endCursor?: string;
+      startCursor?: string;
+    };
+    nodes: GithubIssuesNode[];
+  };
+};
+
+export type IssuesByState = {
+  open: Issues;
+  closed: Issues;
 };
 
 @Injectable({
   providedIn: "root",
 })
 export class GithubService {
-  private token?: string;
+  private API_TOKEN_ITEM = "github-api-token";
+
+  private epics: string[] = [];
+  private issueByEpic: IssueByEpicMap = {};
+  private issues: Issues = {
+    kinds: [],
+    areas: [],
+    byKind: {},
+    byArea: {},
+  };
+  private issuesByState: IssuesByState = {
+    open: {
+      kinds: [],
+      areas: [],
+      byKind: {},
+      byArea: {},
+    },
+    closed: {
+      kinds: [],
+      areas: [],
+      byKind: {},
+      byArea: {},
+    },
+  };
+
+  private epicsSubject: BehaviorSubject<Epics> = new BehaviorSubject<Epics>({
+    epics: this.epics,
+    issuesByEpic: this.issueByEpic,
+  });
+  private issuesSubject: BehaviorSubject<Issues> = new BehaviorSubject<Issues>(
+    this.issues,
+  );
+  private byStateSubject: BehaviorSubject<IssuesByState> =
+    new BehaviorSubject<IssuesByState>(this.issuesByState);
 
   public constructor(private http: HttpClient) {
     this.fetchInformation();
   }
 
-  private fetchInformation() {}
+  private fetchInformation() {
+    this.fetchEpicsInformation().then(() => {});
+    this.fetchIssuesInformation().then(() => {});
+    this.fetchAllIssues().then(() => {});
+  }
+
+  private async fetchEpicsInformation() {
+    if (!this.hasToken()) {
+      return;
+    }
+
+    const token = this.getToken();
+    console.log("token: ", token);
+    const res: GithubSearchResult = await graphql(
+      `
+        {
+          search(
+            type: ISSUE
+            first: 100
+            query: "repo:aquarist-labs/s3gw is:open"
+          ) {
+            issueCount
+            nodes {
+              ... on Issue {
+                title
+                number
+                projectItems(first: 100) {
+                  nodes {
+                    fieldValueByName(name: "Epic") {
+                      ... on ProjectV2ItemFieldSingleSelectValue {
+                        # field
+                        name
+                        # nameHTML
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `,
+      {
+        headers: {
+          authorization: `token ${token}`,
+        },
+      },
+    );
+    res.search.nodes.forEach((entry: GithubSearchNode) => {
+      if (
+        entry.projectItems.nodes.length == 0 ||
+        !entry.projectItems.nodes[0].fieldValueByName
+      ) {
+        return;
+      }
+      const epic = entry.projectItems.nodes[0].fieldValueByName.name;
+      if (!this.epics.includes(epic)) {
+        this.epics.push(epic);
+        this.issueByEpic[epic] = [];
+      }
+      this.issueByEpic[epic].push({
+        number: entry.number,
+        title: entry.title,
+        epic: epic,
+      });
+    });
+    this.epicsSubject.next({
+      epics: this.epics,
+      issuesByEpic: this.issueByEpic,
+    });
+  }
+
+  private async fetchIssuesInformation() {
+    if (!this.hasToken()) {
+      return;
+    }
+    const token = this.getToken();
+    const res: GithubIssuesResult = await graphql(
+      `
+        {
+          search(
+            type: ISSUE
+            first: 100
+            query: "repo:aquarist-labs/s3gw is:open"
+          ) {
+            issueCount
+            nodes {
+              ... on Issue {
+                title
+                number
+                labels(first: 100) {
+                  nodes {
+                    color
+                    name
+                  }
+                }
+                milestone {
+                  title
+                }
+              }
+            }
+          }
+        }
+      `,
+      {
+        headers: {
+          authorization: `token ${token}`,
+        },
+      },
+    );
+    console.debug(res);
+
+    res.search.nodes.forEach((entry: GithubIssuesNode) => {
+      let kind: string = "";
+      let areas: string[] = [];
+
+      entry.labels.nodes.forEach((label: GithubIssueLabel) => {
+        const label_parts = label.name.split("/");
+        if (label_parts.length < 2) {
+          return;
+        }
+        if (label_parts[0] === "kind") {
+          kind = label_parts[1];
+        } else if (
+          label_parts[0] === "area" ||
+          label_parts[0] === "component"
+        ) {
+          areas.push(label_parts[1]);
+        }
+      });
+
+      if (kind === "" && areas.length === 0) {
+        return;
+      }
+
+      const issue: IssueEntry = {
+        title: entry.title,
+        number: entry.number,
+        kind: kind,
+        area: areas,
+      };
+
+      if (kind !== "") {
+        if (!this.issues.kinds.includes(kind)) {
+          this.issues.kinds.push(kind);
+          this.issues.byKind[kind] = [];
+        }
+        this.issues.byKind[kind].push(issue);
+      }
+
+      if (areas.length > 0) {
+        areas.forEach((v: string) => {
+          if (!this.issues.areas.includes(v)) {
+            this.issues.areas.push(v);
+            this.issues.byArea[v] = [];
+          }
+          this.issues.byArea[v].push(issue);
+        });
+      }
+    });
+    this.issuesSubject.next(this.issues);
+  }
+
+  private async fetchAllIssues() {
+    if (!this.hasToken()) {
+      return;
+    }
+    const token = this.getToken();
+
+    const query = `
+    query issues($marker: String = null) {
+      search(type: ISSUE, first: 100, after: $marker,
+             query: "repo:aquarist-labs/s3gw type:issue") {
+        issueCount
+        pageInfo {
+          endCursor
+          startCursor
+        }
+        nodes {
+          ... on Issue {
+            state
+            title
+            number
+            labels(first: 100) {
+              nodes {
+                color
+                name
+              }
+            }
+            milestone {
+              title
+            }
+          }
+        }
+      }
+    }
+    `;
+
+    let done = false;
+    let marker: string | null = null;
+    do {
+      const res: GithubAllIssuesResult = await graphql(query, {
+        marker: marker,
+        headers: {
+          authorization: `token ${token}`,
+        },
+      });
+      if (!res.search.pageInfo.endCursor) {
+        done = true;
+      } else {
+        marker = res.search.pageInfo.endCursor;
+      }
+      console.debug(res.search.nodes);
+      this.handleIssues(res.search.nodes);
+    } while (!done);
+    this.byStateSubject.next(this.issuesByState);
+  }
+
+  private handleIssues(issues: GithubIssuesNode[]) {
+    issues.forEach((entry: GithubIssuesNode) => {
+      if (!entry.state) {
+        console.error("Unexpected stateless issue: ", entry);
+        return;
+      }
+
+      const isOpen = entry.state.toLowerCase() === "open";
+      const target = isOpen
+        ? this.issuesByState.open
+        : this.issuesByState.closed;
+
+      let kind: string = "";
+      let areas: string[] = [];
+
+      entry.labels.nodes.forEach((label: GithubIssueLabel) => {
+        const label_parts = label.name.split("/");
+        if (label_parts.length < 2) {
+          return;
+        }
+        if (label_parts[0] === "kind") {
+          kind = label_parts[1];
+        } else if (
+          label_parts[0] === "area" ||
+          label_parts[0] === "component"
+        ) {
+          areas.push(label_parts[1]);
+        }
+      });
+
+      if (kind === "" && areas.length === 0) {
+        return;
+      }
+
+      const issue: IssueEntry = {
+        title: entry.title,
+        number: entry.number,
+        kind: kind,
+        area: areas,
+      };
+
+      if (kind !== "") {
+        if (!target.kinds.includes(kind)) {
+          target.kinds.push(kind);
+          target.byKind[kind] = [];
+        }
+        target.byKind[kind].push(issue);
+      }
+
+      if (areas.length > 0) {
+        areas.forEach((v: string) => {
+          if (!target.areas.includes(v)) {
+            target.areas.push(v);
+            target.byArea[v] = [];
+          }
+          target.byArea[v].push(issue);
+        });
+      }
+    });
+
+    console.log(this.issuesByState);
+  }
 
   public setToken(token: string) {
-    this.token = token;
+    localStorage.setItem(this.API_TOKEN_ITEM, token);
+    this.fetchEpicsInformation();
+  }
+
+  public getToken(): string {
+    let res = localStorage.getItem(this.API_TOKEN_ITEM);
+    console.assert(!!res);
+    return res!;
   }
 
   public hasToken(): boolean {
-    return !!this.token;
+    let res = localStorage.getItem(this.API_TOKEN_ITEM);
+    return !!res;
+  }
+
+  public getEpics(): BehaviorSubject<Epics> {
+    return this.epicsSubject;
+  }
+
+  public getIssues(): BehaviorSubject<Issues> {
+    return this.issuesSubject;
+  }
+
+  public getIssuesByState(): BehaviorSubject<IssuesByState> {
+    return this.byStateSubject;
   }
 }
